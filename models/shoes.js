a0c9a2770f8d30b350b4a3027aeb3269abe0d61a
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2023-11-29 17:21:14 -0600

    commit change

--- models/shoes.js
+++ models/shoes.js
@@ -1,8 +1,25 @@
 const mongoose = require("mongoose")
 const shoesSchema = mongoose.Schema({
-Brand: String,
-Ambassador: String,
-price: String
+Brand :
+{
+    type: String,
+    minlength: 3,
+    maxlength: 10,
+
+},
+Ambassador: 
+{
+    type: String,
+    minlength: 1,
+    maxlength: 100,
+},
+price: 
+{
+    type: String,
+    minlength: 1,
+    maxlength: 10,
+},
+
 })
 module.exports = mongoose.model("shoe", shoesSchema)
 
--- routes/shoes.js
+++ routes/shoes.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const shoe_controllers= require('../controllers/shoes');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+    
 /* GET mobile */
 router.get('/', shoe_controllers.shoe_view_all_Page );
 module.exports = router;
@@ -11,10 +18,10 @@ router.get('/shoes/:id', shoe_controllers.shoe_detail);
 router.get('/detail', shoe_controllers.shoe_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', shoe_controllers.shoe_create_Page);
+router.get('/create', secured, shoe_controllers.shoe_create_Page);
 
 /* GET create update page */
-router.get('/update', shoe_controllers.shoe_update_Page);
+router.get('/update',secured, shoe_controllers.shoe_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', shoe_controllers.shoe_delete_Page);
+router.get('/delete', secured, shoe_controllers.shoe_delete_Page);
